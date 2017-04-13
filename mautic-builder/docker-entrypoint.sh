e5df907feb2d3685130b7b843a35c2fca799aff6
Alan Pazetto <alancpazetto@gmail.com>
2017-04-13 14:42:35 -0300

    Test

--- mautic-builder/docker-entrypoint.sh
+++ mautic-builder/docker-entrypoint.sh
@@ -67,8 +67,8 @@ if ! [ -e app/config/local.php ]; then
 fi
 
 if [[ "$MAUTIC_RUN_CRON_JOBS" == "true" ]]; then
-    mkdir /var/spool
-    mkdir /var/spool/cron
+    # mkdir /var/spool
+    mkdir -p /var/spool/cron
     echo >&2
     echo >&2 "Running cron."
     if [ ! -e /var/log/cron.pipe ]; then
