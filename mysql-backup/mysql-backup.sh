522482781c143ef7ae88b9ea549548c528e6cc94
Alan Pazetto <alancpazetto@gmail.com>
2017-04-15 15:41:45 -0300

    Tweak mysql backup

--- mysql-backup/mysql-backup.sh
+++ mysql-backup/mysql-backup.sh
@@ -4,15 +4,15 @@
 CONTAINER_NAME="nhrd.mautic.db"
 MYSQL_ROOT_PASSWORD="XiNCSiuVqK34"
 
-docker exec $CONTAINER_NAME /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD mautic > backup.sql # Backup
 mkdir -p $CONTAINER_NAME
-cat ./$CONTAINER_NAME/backup.sql | docker exec -i $CONTAINER_NAME /usr/bin/mysql -u root --password=$MYSQL_ROOT_PASSWORD mautic # Restore
+docker exec $CONTAINER_NAME /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD mautic > ./$CONTAINER_NAME/backup.sql # Backup
+# cat ./$CONTAINER_NAME/backup.sql | docker exec -i $CONTAINER_NAME /usr/bin/mysql -u root --password=$MYSQL_ROOT_PASSWORD mautic # Restore
 
 
 #ILE DE PIPA
 CONTAINER_NAME="iledepipa.mautic.db"
 MYSQL_ROOT_PASSWORD="NB54Cp6Vb92d"
 
-docker exec $CONTAINER_NAME /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD mautic > backup.sql # Backup
 mkdir -p $CONTAINER_NAME
-cat ./$CONTAINER_NAME/backup.sql | docker exec -i $CONTAINER_NAME /usr/bin/mysql -u root --password=$MYSQL_ROOT_PASSWORD mautic # Restore
+docker exec $CONTAINER_NAME /usr/bin/mysqldump -u root --password=$MYSQL_ROOT_PASSWORD mautic > ./$CONTAINER_NAME/backup.sql # Backup
+# cat ./$CONTAINER_NAME/backup.sql | docker exec -i $CONTAINER_NAME /usr/bin/mysql -u root --password=$MYSQL_ROOT_PASSWORD mautic # Restore
